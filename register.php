58695a425f269a407265d6660f4c791d7cde0782
ChadS <Chad_smith95@yahoo.com>
2019-05-05 05:31:58 -0400

    Register page edit -Added submit button. -removed placeholders. -Changed address to zipcode. -Styled register page.

--- register.php
+++ register.php
@@ -5,43 +5,63 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="CSS/register.css" rel="stylesheet">
-        
-    
-    </head>
-    <body>
-        <h1 id ="title">Create Account</h1>
 
-    <form action="POST">
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="CSS/register.css" rel="stylesheet">
 
-        <label class="heading">First Name*</label>
-        <input type="text" placeholder="Enter First Name" name="first" class="textbox" required ><br>
+
+</head>
+
+<body>
+    <h1 id="title">Create Account</h1>
+    <div>
+
+
+        <form action="POST" class="form">
+
+            <!-- <label class="heading">First Name*</label>
+        <input type="text"  name="first" class="textbox" required><br>
 
 
         <label class="heading">Last Name*</label>
-        <input type="text" placeholder="Enter Last Name" name="last" class="textbox" required> <br>
-        
+        <input type="text"  name="last" class="textbox" required> <br>
+
         <label class="heading">Username*</label>
-        <input type="text" placeholder="Enter Username" name="user" class="textbox" required>  <br>
+        <input type="text"  name="user" class="textbox" required> <br>
 
         <label class="heading">Password*</label>
-        <input type="password" placeholder="Enter Password" name="pass" class="textbox" required><br>
-        
-       
-        <label class="heading">Address*</label>
-        <input type="text" placeholder="Enter Address" name="address1" required><br>
-        <input type="text" placeholder="Enter City, State, Zipcode" name="address2" required>
-        
-       
-        
-       
-        
-        
-    </form>
+        <input type="password"  name="pass" class="textbox" required><br>
 
-    </body>
+
+        <label class="heading">Zipcode*</label>
+        <input type="text"  name="address1" required><br> -->
+            <!-- <input type="text" placeholder="Enter City, State, Zipcode" name="address2" required> -->
+
+            <!-- <input type="submit" value="Submit"> -->
+            <label for="firstname">First Name*</label>
+            <input type="text" id="fname" name="firstname" required><br>
+
+            <label for="lastname">Last Name*</label>
+            <input type="text" id="lname" name="lastname"  required><br>
+
+            <label for="email">Email*</label>
+            <input type="email" name="email" id="email" required><br>
+
+            <label for="country">Zipcode*</label>
+            <input type="text" name="zipcode" id="zipcode" required><br>
+
+            <input type="submit" value="Submit">
+
+
+
+
+
+
+        </form>
+    </div>
+</body>
+
 </html>
