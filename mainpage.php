0d77039545c48eeefa5b5be73bece314ef832423
ChadS <Chad_smith95@yahoo.com>
2019-05-05 06:06:54 -0400

    Added info -added parking page -images float -added hover effect & opacity

--- mainpage.php
+++ mainpage.php
@@ -1,18 +1,30 @@
 <?php
     session_start();//starts session.
-    include();// TODO: include navbar 
-    include();//TODO: inlcude database file db_Cconnect
+    // include();// TODO: include navbar 
+    // include();//TODO: inlcude database file db_Cconnect
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Homepage</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
-    </body>
+
+<head>
+    <title>Homepage</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="CSS/mainpage.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="pic">
+        <a href="pre-parking.php">
+            <img src="images/parking.jpg">
+        </a>
+
+        <!-- TODO: add other part of website -->
+        <!-- <a href="placeholder.txt">
+            <img src="images/parking2.jpg"> -->
+        </a>
+    </div>
+</body>
+
 </html>
