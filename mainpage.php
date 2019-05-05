28595ec131722283bf2fce5ef8f183998882a75d
ChadS <Chad_smith95@yahoo.com>
2019-05-05 05:39:32 -0400

    Created homepage -Navigates the homepage between parking and other pages

---
+++ mainpage.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();//starts session.
+    include();// TODO: include navbar 
+    include();//TODO: inlcude database file db_Cconnect
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Homepage</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+    </body>
+</html>
